5a775a0fb0e99bd961b7b4e0578544a9eaf73663
mbleichert <M.Bleichert@meteovista.nl>
2017-02-25 18:55:13 +0100

    adding boilerplate for csv tests

--- tests/parser-test.js
+++ tests/parser-test.js
@@ -1,30 +1,46 @@
 import { expect } from 'chai';
 import parser from '../app/components/parser.js';
-import xmlString from './data.js';
+import { xml as xmlString } from './data.js';
 
-describe('parse xml string to json', () => {
-  it('should return an error when input is null', (done) => {
-    parser.parseXml(null, (err, data) => {
-      expect(err).to.be.not.equal(null);
-      expect(data).to.be.equal(undefined);
-      done();
+describe('parse data', () => {
+  describe.skip('parse csv string to json', () => {
+    it('should return an error when input is null', () => {
+      // not implemented yet
+    });
+
+    it('should return an error when input is not a valid csv string', () => {
+      // not implemented yet
+    });
+
+    it('should parse csv string to json', () => {
+      // not implemented yet
     });
   });
 
-  it('should return an error when input is not a valid xml string', (done) => {
-    parser.parseXml('<a>bad xml<b>!</b></a>', (err, data) => {
-      expect(err).to.be.not.equal(null);
-      expect(data).to.be.equal(undefined);
-      done();
+  describe('parse xml string to json', () => {
+    it('should return an error when input is null', (done) => {
+      parser.parseXml(null, (err, data) => {
+        expect(err).to.be.not.equal(null);
+        expect(data).to.be.equal(undefined);
+        done();
+      });
     });
-  });
 
-  it('should parse xml string to json', (done) => {
-    parser.parseXml(xmlString, (err, data) => {
-      expect(err).to.be.equal(null);
-      expect(data).to.be.an('array');
-      expect(data.length).to.be.equal(11);
-      done();
+    it('should return an error when input is not a valid xml string', (done) => {
+      parser.parseXml('<a>bad xml<b>!</b></a>', (err, data) => {
+        expect(err).to.be.not.equal(null);
+        expect(data).to.be.equal(undefined);
+        done();
+      });
+    });
+
+    it('should parse xml string to json', (done) => {
+      parser.parseXml(xmlString, (err, data) => {
+        expect(err).to.be.equal(null);
+        expect(data).to.be.an('array');
+        expect(data.length).to.be.equal(11);
+        done();
+      });
     });
   });
 });
